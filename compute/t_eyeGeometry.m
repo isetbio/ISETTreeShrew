a4672f6a742f02dae000a85910a4e0117ffeda48
Jared Collina <jared@nicolass-iMac.local>
2019-02-11 15:37:25 -0500

    Further documentation formatting

--- compute/t_eyeGeometry.m
+++ compute/t_eyeGeometry.m
@@ -2,10 +2,10 @@
 % Illustrate basic principles of how eye parameters affect isomerizations.
 %
 % Description:
-%    Demonstrate how changing the focal length, the pupil diameter 
+%    Demonstrate how changing the focal length, the pupil diameter
 %    and the inner segment aperture affect photoreceptor isomerization rate
 %    computed by ISETBio, and compare this with the analytical analysis presented
-%    Animal Eyes (ADD BIBLIGRAPHIC INFO), pages XX ff.
+%    in Animal Eyes (Land & Nilsson, 2002), pages 51 ff.
 %
 % See also:
 %
@@ -13,6 +13,7 @@
 % History:
 %   02/01/19 jsc  Wrote initial version.
 %   02/08/19 jsc, dhb Documentation formatting etc.
+%   02/11/19 jsc, Further documentation formatting
 
 %% Initialize workspace and close old figures
 clear; close all;
@@ -30,7 +31,7 @@ basePupilDiameterMM = 2.0;
 baseInnerSegmentDiameterUM = 7.0;
 
 % Level of change of varied parameter.  This also gets scaled
-% by sqrt of integer change before being added to the base. 
+% by sqrt of integer change before being added to the base.
 % Numerical choices are not fundamental, just to give us a reasonable
 % range.
 deltaFocalLengthMM = 2.0;
@@ -38,19 +39,19 @@ deltaPupilDiameterMM = 1.0;
 deltaInnerSegmentDiameterUM = 3.0;
 
 % Specify which eye parameter we'll study in this run. The way this works
-% is that the vector specifise which of the parameters will be incremented
+% is that the vector specifies which of the parameters will be incremented
 % in the main loop below, where we recompute isomerizations across eye
 % parameter variation.
 %   [1,0,0] - Vary focal length
 %   [0,1,0] - Vary pupil diameter
 %   [0,0,1] - Vary inner segment diameter
-v = [1,0,0];
-v = [0,0,1];
+
+v = [0,1,0];
 
 % Specify cone densities similar to Peichl 1989 and set up a mosaic. Tree
 % shrew mosaic dominated by L cones, so  we'll approximate with only L cones.
-% For historical reasons, ISETBio parameters cone types in a vector
-% "black", L, M and S. Tree shrews have no M cones. 
+% For historical reasons, ISETBio parameters cone types formatted in a vector
+% "black", L, M and S. Tree shrews have no M cones.
 %
 % Variable whichConeType determines which cone type we'll use
 % to estimate isomerizations.  2 -> L, 3 -> M 4 -> S.  It would
@@ -83,26 +84,27 @@ testScene = sceneCreate('uniformEqualPhoton');
 % Compute isomerizations across parameter variation and save up the results
 % for plotting.
 for n = 1:nPointsToCompute
-    % Get a vector that lets us decide the size of each of the parameters.  This
+    % Get a vector that lets us decide the size of each of the parameters. This
     % starts with v and increments on each loop iteration.
     vector = [n-1,n-1,n-1].*v;
     
-    % Set up parameters for this itereation. We vary delta according to
+    % Set up parameters for this iteration. We vary delta according to
     % sqrt of iteration for each parameter, just because we like the way
     % plots look when we do that.
     focalLengthMM = baseFocalLengthMM + sqrt(vector(1)) * deltaFocalLengthMM;
     pupilDiameterMM = basePupilDiameterMM + sqrt(vector(2)) * deltaPupilDiameterMM;
-    innerSegmentDiameterUM = baseInnerSegmentDiameterUM + sqrt(vector(3)) * deltaInnerSegmentDiameterUM;
+    innerSegmentDiameterUM = baseInnerSegmentDiameterUM + sqrt(vector(3)) * ...
+        deltaInnerSegmentDiameterUM;
     
     % This switch statement sets up information for plotting, which depends
     % on which eye parameter we are varying. We also store the value of the
-    % parameter for each interation, to be used in labeling individual
+    % parameter for each iteration, to be used in labeling individual
     % points in the plot.
     switch find(v)
         case 1
             eyeParameterValue(n) = focalLengthMM;
-            parameterName = 'Focal Length'; 
-            shortParameterName = 'F'; 
+            parameterName = 'Focal Length';
+            shortParameterName = 'F';
             parameterUnits = 'mm';
         case 2
             eyeParameterValue(n) = pupilDiameterMM;
@@ -145,39 +147,18 @@ for n = 1:nPointsToCompute
         meanResponseToOpticalImage(tMosaic, tMosaicExcitation, whichConeType);
     
     % Calculate estimated sensitivity according to Animal Eyes
-    s_AnimalEyes(n) = 0.62 * (pupilDiameterMM^2 * innerSegmentDiameterUM^2)/ ...
+    s_AnimalEyes(n) = (pupilDiameterMM^2 * innerSegmentDiameterUM^2)/ ...
         (focalLengthMM^2);
-    
 end
 
-s_Iset = tMosaicExcitationMean;
+%% Visualizing correlation between measures of sensitivity
+
 x = s_AnimalEyes;
-y = s_Iset;
-ft = fitlm(x,y);
+y = tMosaicExcitationMean;
 
-plot(x,y,'o')
-plot(ft)
-xlabel('Sensitivity (Animal Eyes)')
-ylabel('Sensitivty (ISETBio)')
-xlim([0,inf])
-ylim([0,inf])
-
-title([{'Relationship Between ISETBIO and Animal Eyes Sensitivity'}, ...
-    {sprintf('As %s Changes',parameterName)}])
-
-labels = cell(1,nPointsToCompute);
-for i=1:nPointsToCompute
-labels(i) = cellstr(sprintf('%s= %.2f %s', shortParameterName, eyeParameterValue(i), parameterUnits));
-end
-
-text(x,y,labels,'VerticalAlignment','top','HorizontalAlignment','right')
-
-I1 = meanRetinalIlluminance./meanRetinalIlluminance(1); %illuminance for each level
-
-E1 = tMosaicExcitationMean./tMosaicExcitationMean(1); %efficiency for each cone (LMS) for each level
-
-S1 = s_Iset./s_Iset(1);
-
+% Plot sensitivities and best-fit line
+plotSens(x,y,parameterName,nPointsToCompute,shortParameterName,eyeParameterValue, ...
+    parameterUnits)
 
 
 %% Functions
@@ -188,4 +169,27 @@ nTrialsNum = size(coneMosaicResponse,1);
 coneMosaicResponse  = reshape(coneMosaicResponse, [nTrialsNum numel(coneMosaic.pattern)]);
 idx = find(coneMosaic.pattern == targetConeType);
 meanResponse = mean(mean(coneMosaicResponse(:,idx)));
+end
+
+function plotSens(x,y,parameterName,nPointsToCompute,shortParameterName, ...
+    eyeParameterValue,parameterUnits)
+ft = fitlm(x,y);
+xnew = [0,max(x)];
+ypred = predict(ft,xnew');
+hold on
+plot(x,y,'o')
+plot(xnew,ypred)
+xlabel('Sensitivity (Animal Eyes)')
+ylabel('Sensitivty (ISETBio)')
+xlim([0,max(x)])
+ylim([0,max(y)])
+title([{'Relationship Between ISETBIO and Animal Eyes Sensitivity'}, ...
+    {sprintf('As %s Changes',parameterName)}])
+labels = cell(1,nPointsToCompute);
+for i=1:nPointsToCompute
+    labels(i) = cellstr(sprintf('%s= %.2f %s', shortParameterName, ...
+        eyeParameterValue(i), parameterUnits));
+end
+text(x,y,labels,'VerticalAlignment','top','HorizontalAlignment','right')
+hold off
 end
